e66d7c5d72e7508b5e43e644076047d6e3cdc6dc
parahaoer <884101054@qq.com>
2021-02-05 14:43:23 +0800

    update

---
+++ 13/13.11.2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void storageChar(char *a, int n) {
+    
+    // 读取输入n后面的一个空格来跳过它
+    char ch = getchar();
+    int i = 0;
+    for (; i < n && (ch = getchar()) && ch != ' ' && ch != '\t' && ch != '\n'; i++) {
+        a[i] = ch;
+    }
+    a[i] = '\0';
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    char a[n + 1];
+    storageChar(a, n);
+    printf("%s", a);
+    return 0;
+}
